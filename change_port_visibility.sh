87043cb5cc64eddac198f53291d87651742dfdf5
Ryan R. Fox <ryanRfox@users.noreply.github.com>
2023-10-02 08:30:51 -0400

    make ports public

--- change_port_visibility.sh
+++ change_port_visibility.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-gh codespace ports visibility $1:$2 -c $CODESPACE_NAME
+echo "making port 4001 public"
+gh codespace ports visibility 4001:public -c $CODESPACE_NAME
+echo "making port 4002 public"
+gh codespace ports visibility 4002:public -c $CODESPACE_NAME
+echo "making port 8980 public"
+gh codespace ports visibility 8980:public -c $CODESPACE_NAME
